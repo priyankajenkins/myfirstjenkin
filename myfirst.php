3042b9c23045f9e926f001ca0207e94db50fe768
priyanka.attarde <priyanka.attarde@infobeans.com>
2023-08-21 16:14:45 +0530

    updated

--- myfirst.php
+++ myfirst.php
@@ -1,11 +1,7 @@
 <?php
 
 echo "hello<br>";
-                                                                        echo "This is my first jenkin configuration- update3";
-        echo "executed job2 as well take 1.";
-                echo "executed job2 as well take 3.";;
-if($test){
-echo "here with new update";
- }
+echo "This is my first jenkin configuration- update3";
+
 
 
