df4f88f9b3a20b72a436366743a0f8205b1f39ec
priyanka.attarde <priyanka.attarde@infobeans.com>
2023-08-23 19:18:16 +0530

    updated1

--- myfirst.php
+++ myfirst.php
@@ -4,7 +4,8 @@ echo "hello<br>1";
 echo "This is my first jenkin configuration- update3";
     echo "new test"; 
         //if
-//test
+/*test*/
 
 echo "test at Agu 23";
 echo "test2";
+echo "test3";
